7f1c39c27fcb4acfbd60a9d84c5e2164eaf9b6c3
AlbertZJ <1353296585@qq.com>
2020-01-13 03:30:04 +0800

    init project

--- src/main/java/com/gm/wj/controller/LibraryController.java
+++ src/main/java/com/gm/wj/controller/LibraryController.java
@@ -68,6 +68,7 @@ public class LibraryController {
 //            System.out.println(file.getOriginalFilename());
 //            System.out.println("http://localhost:8443/api/file/" + f.getName());
 
+
           // 本地地址添加
             // 分支添加本地地址
            String imgURL = "http://localhost:8087/api/file/" + f.getName();
